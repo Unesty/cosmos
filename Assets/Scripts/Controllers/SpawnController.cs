030769d66d9324c87759cd5ff68d81cfd9a0b6f6
Unesty <unesty@protonmail.com>
2020-04-14 15:59:35 +0400

    добавил новую систему спавна

--- Assets/Scripts/Controllers/SpawnController.cs
+++ Assets/Scripts/Controllers/SpawnController.cs
@@ -29,8 +29,8 @@ public class SpawnController : MonoBehaviour
     private float timeMeteoriteSpawn = 2;
     private float timeMeteoriteWait = 2;
 
-    private float timeShieldSpawn = 5;
-    private float timeShieldWait = 3;
+    private float timeShieldSpawn = 15;
+    private float timeShieldWait = 9;
 
     private float timeBalloonSpawn = 6;
     private float timeBalloonWait = 1;
@@ -40,11 +40,18 @@ public class SpawnController : MonoBehaviour
 
     // Можно ли спаунить на определённой линии
     private bool[] timeSpawnOnLine = new bool[]{false, false, false};
-    
+
     private bool createItemNow;
 
     private float lifetime = 0;
     public float spawnRateDiv = 1000;
+
+    public float itemsPerMinute = 100;
+    public Vector2 starChance;
+    public Vector2 meteoriteChance;
+    public Vector2 shieldChance;
+    public Vector2 balloonChance;
+    public Vector2 anyChance;
 
     private void Awake()
     {
@@ -56,6 +63,7 @@ public class SpawnController : MonoBehaviour
     {
         if (!stopGame)
         {
+            /*
             if (timeMeteoriteWait <= Random.Range(0f,lifetime/spawnRateDiv))
             {
                 timeMeteoriteWait = timeMeteoriteSpawn;
@@ -106,15 +114,52 @@ public class SpawnController : MonoBehaviour
             timeShieldWait -= Time.deltaTime;
             timeAnyWait -= Time.deltaTime;
             lifetime += Time.deltaTime;
+            */
+            bool rndItem = Random.Range(0f,3600f/(float)itemsPerMinute)<1f;
+            if(rndItem){
+                float rnd=Random.Range(0f,1f);
+                if(rnd>starChance[0]&&rnd<starChance[1]){
+                    StartCoroutine(CreateStar());
+                }
+                if(rnd>meteoriteChance[0]&&rnd<meteoriteChance[1]){
+                    StartCoroutine(CreateMeteorite());
+                }
+                if(rnd>shieldChance[0]&&rnd<shieldChance[1]){
+                    StartCoroutine(CreateShield());
+                }
+                if(rnd>balloonChance[0]&&rnd<balloonChance[1]){
+                    StartCoroutine(CreateBalloon());
+                }
+                if(rnd>anyChance[0]&&rnd<anyChance[1]){
+                    switch(Random.Range(0,4)){
+                    case 0:
+                        StartCoroutine(CreateMeteorite());
+                        break;
+                    case 1:
+                        StartCoroutine(CreateStar());
+                        break;
+                    case 2:
+                        StartCoroutine(CreateShield());
+                        break;
+                    case 3:
+                        StartCoroutine(CreateBalloon());
+                        break;
+                    }
+                }
+            }
         }
     }
 
+    //Создание астероида
     private IEnumerator CreateMeteorite()
     {
         if (createItemNow)
             yield return new WaitForSeconds(.2f);
 
         int rnd = CheckStartPositions();
+        if(rnd==-1){
+            yield break;
+        }
 
         Meteorite newMeteorite = Instantiate(meteoritePrefab, startPositions[rnd].transform.position, Quaternion.identity);
         newMeteorite.spawnCon = GetComponent<SpawnController>();
@@ -131,6 +176,9 @@ public class SpawnController : MonoBehaviour
             yield return new WaitForSeconds(.2f);
 
         int rnd = CheckStartPositions();
+        if(rnd==-1){
+            yield break;
+        }
 
         Star newStar = Instantiate(starPrefab, startPositions[rnd].transform.position, Quaternion.identity);
         newStar.spawnCon = GetComponent<SpawnController>();
@@ -149,6 +197,9 @@ public class SpawnController : MonoBehaviour
             yield return new WaitForSeconds(.2f);
 
         int rnd = CheckStartPositions();
+        if(rnd==-1){
+            yield break;
+        }
 
         Shield newShield = Instantiate(shieldPrefab, startPositions[rnd].transform.position, Quaternion.identity);
         newShield.spawnCon = GetComponent<SpawnController>();
@@ -165,6 +216,9 @@ public class SpawnController : MonoBehaviour
             yield return new WaitForSeconds(.2f);
 
         int rnd = CheckStartPositions();
+        if(rnd==-1){
+            yield break;
+        }
 
         Balloon newBalloon = Instantiate(balloonPrefab, startPositions[rnd].transform.position, Quaternion.identity);
         newBalloon.spawnCon = GetComponent<SpawnController>();
@@ -179,15 +233,72 @@ public class SpawnController : MonoBehaviour
     {
         createItemNow = true;
 
-        int rnd = Random.Range(0, startPositions.Length);
+        //int rnd = Random.Range(0, startPositions.Length);
+        /*
         if (timeSpawnOnLine[rnd]) //while may cause freeze
         {
             rnd = Random.Range(0, startPositions.Length);
+            
         }
-
-        StartCoroutine(CoTimerSpawnOnLive(rnd));
-        
-        return rnd;
+        */
+        /*
+        int fpc=0;
+        for(int n=0;n<timeSpawnOnLine.Length;n++){
+            fpc+=timeSpawnOnLine[n]==false;
+        }
+        */
+        if(timeSpawnOnLine[0]){ //this is definetly not a good code
+            if(timeSpawnOnLine[1]){
+                if(timeSpawnOnLine[2]){ 
+                    return -1;
+                }else{
+                    StartCoroutine(CoTimerSpawnOnLive(2));
+                    return 2;
+                }
+            }else{
+                if(timeSpawnOnLine[2]){
+                    StartCoroutine(CoTimerSpawnOnLive(1));
+                    return 1;
+                }else{
+                    if(1==Random.Range(0, 1)){
+                        StartCoroutine(CoTimerSpawnOnLive(1));
+                        return 1;
+                    }else{
+                        StartCoroutine(CoTimerSpawnOnLive(2));
+                        return 2;
+                    }
+                }
+            }
+        }else{
+            if(timeSpawnOnLine[1]){
+                if(timeSpawnOnLine[2]){
+                        StartCoroutine(CoTimerSpawnOnLive(0));
+                        return 0;
+                }else{
+                    if(1==Random.Range(0, 1)){
+                        StartCoroutine(CoTimerSpawnOnLive(0));
+                        return 0;
+                    }else{
+                        StartCoroutine(CoTimerSpawnOnLive(2));
+                        return 2;
+                    }
+                }
+            }else{
+                if(timeSpawnOnLine[2]){
+                    if(1==Random.Range(0, 1)){
+                        StartCoroutine(CoTimerSpawnOnLive(0));
+                        return 0;
+                    }else{
+                        StartCoroutine(CoTimerSpawnOnLive(1));
+                        return 1;
+                    }
+                }else{
+                        int rnd=Random.Range(0, 2);
+                        StartCoroutine(CoTimerSpawnOnLive(rnd));
+                        return rnd;
+                }
+            }
+        }
     }
 
     // Запрет на спаун на 1 секунду на линии текущего спауна
@@ -195,7 +306,7 @@ public class SpawnController : MonoBehaviour
     {
         timeSpawnOnLine[numberLine] = true;
 
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(60f/(float)itemsPerMinute);
 
         timeSpawnOnLine[numberLine] = false;
     }
