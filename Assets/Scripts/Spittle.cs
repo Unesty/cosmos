2cc3e0d2b37cd2dbcacb532bb9611673306e19b3
aleksanderConushin <aleksander.conushin@yandex.ru>
2020-04-14 23:26:26 +0300

    1. Переименовал спрайты 2. Добавил больше комментариев

---
+++ Assets/Scripts/Spittle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spittle : Item
+{
+    protected override void OnTriggerEnter2D(Collider2D collision)
+    {
+        base.OnTriggerEnter2D(collision);
+
+        if (collision.gameObject.tag == "EndSpace")
+            DeathObject(GetComponent<Item>());
+    }
+
+    protected override void InteractionWithPlayer(GameObject collision)
+    {
+        base.InteractionWithPlayer(collision);
+
+        collision.gameObject.GetComponent<PlayerController>().Death();
+
+        DeathObject(GetComponent<Item>());
+    }
+}
